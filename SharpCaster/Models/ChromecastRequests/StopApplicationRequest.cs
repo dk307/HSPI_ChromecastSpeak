4d316d80c082291b28d1cba092ba5ea9d756d58f
dk307 <9539519+dk307@users.noreply.github.com>
2018-12-31 15:54:32 -0800

    Stoping playback is detected during speak.

---
+++ SharpCaster/Models/ChromecastRequests/StopApplicationRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace SharpCaster.Models.ChromecastRequests
+{
+    [DataContract]
+    public class StopApplicationRequest : RequestWithId
+    {
+        public StopApplicationRequest(string sessionId, int requestId)
+            : base("STOP", requestId)
+        {
+            SessionId = sessionId;
+        }
+
+        [DataMember(Name = "sessionId")]
+        public string SessionId { get; set; }
+    }
+}
